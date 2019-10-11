f3738ba94b2de1508ba9aadfbf0556f2284c43f2
Christian Oliveira <31349594+Christian-Oliveira@users.noreply.github.com>
2019-10-10 21:43:50 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define( 'DB_NAME', 'bd_igq' );
+define( 'DB_NAME', 'igq' );
 
 /** Usuário do banco de dados MySQL */
 define( 'DB_USER', 'root' );
